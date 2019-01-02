7fcb42e31122878f46e389717329be3b320e2ad6
scwang18 <45680124+scwang18@users.noreply.github.com>
2019-01-02 09:08:25 +0000

    tdd2

--- odoo_dev_traning/tests/test_lesson.py
+++ odoo_dev_traning/tests/test_lesson.py
@@ -13,12 +13,12 @@ class TestLesson(TransactionCase):
         """
         lesson = self.env['training.lesson'].create({
             'name': '英语会话',
-            'teacher_id': self.env.ref('pscloud_train.demo_training_subject').id,
+            'teacher_id': self.env.ref('odoo_dev_traning.demo_training_subject').id,
             'start_date': '2018-01-01',
             'end_date': '2018-07-01',
             'seat_qty': 30,
-            'subject_id': self.env.ref('pscloud_trainging.demo_trainging_subject').id,
-            'student_ids': [(4, self.env.ref('pscloud_trainging.demo_training_student_1').id)],
+            'subject_id': self.env.ref('odoo_dev_traning.demo_trainging_subject').id,
+            'student_ids': [(4, self.env.ref('odoo_dev_traning.demo_training_student_1').id)],
         })
         
         lesson.action_confirm()
